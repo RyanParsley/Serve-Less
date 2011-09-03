b7b0010746ca2c1649c636028020007aa35ade4f
Ryan Parsley <RMParsley@gmail.com>
2011-09-03 12:00:02 -0400

    first commit

---
+++ views/view_helpers.rb
@@ -0,0 +1,33 @@
+#
+# Methods placed in this module can be used inside of any view.
+# View helpers allow you to encapsalate complex logic and keep your
+# views pristine.
+#
+module ViewHelpers
+  
+  # Calculate the years for a copyright
+  def copyright_years(start_year)
+    end_year = Date.today.year
+    if start_year == end_year
+      "\#{start_year}"
+    else
+      "\#{start_year}&#8211;\#{end_year}"
+    end
+  end
+  
+  # Handy for hiding a block of unfinished code
+  def hidden(&block)
+    #no-op
+  end
+  
+  # Add your own helpers below...
+ 
+  def path
+    URI.parse(request.request_uri).path
+  end
+  
+  def active?(part)
+     "active" if (@selected == part) or Regexp.new(part).match(path)
+  end
+  
+end
